40821b287ece69fd0cac28af1fd1bf3bf1cee751
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-05-16 09:46:50 +0700

    splash screen slicing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/pages/signin_page.dart';
+import 'package:shamo/pages/splash_page.dart';
 import 'theme.dart';
 
 void main() => runApp(MyApp());
@@ -8,17 +10,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Halo Semuanya!', style: priceTextStyle,
-      ),),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashPage(),
+        '/sign-in': (context) => SignInPage(),
+      }
     );
   }
 }
