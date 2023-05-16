a3abbf7960e68cc89dd18007e9d3667b23c96365
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-05-16 14:16:22 +0700

    Home Page Header Slicing

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -4,13 +4,54 @@ import 'package:shamo/theme.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Home Page',
-          style: primaryTextStyle,
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin,
         ),
-      ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Halo, Jerry!',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semibold,
+                    ),
+                  ),
+                  Text(
+                    '@jerryandriantop',
+                    style: subtitleTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('assets/image_profile.png'),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
