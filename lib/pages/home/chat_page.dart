05ba546bda4d9dc51986266cee62794c0dcf88cc
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-05-17 08:31:33 +0700

    content chat slicing

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
+import 'package:shamo/widgets/chat_tile.dart';
 
 class ChatPage extends StatelessWidget {
   @override
@@ -20,7 +21,7 @@ class ChatPage extends StatelessWidget {
       );
     }
 
-    Widget content() {
+    Widget emptyChat() {
       return Expanded(
         child: Container(
           width: double.infinity,
@@ -57,14 +58,15 @@ class ChatPage extends StatelessWidget {
                 child: TextButton(
                   onPressed: () {},
                   style: TextButton.styleFrom(
-                      padding: EdgeInsetsDirectional.symmetric(
-                        horizontal: 24,
-                        vertical: 10,
-                      ),
-                      backgroundColor: primaryColor,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      )),
+                    padding: EdgeInsetsDirectional.symmetric(
+                      horizontal: 24,
+                      vertical: 10,
+                    ),
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
                   child: Text(
                     'Explore Store',
                     style: primaryTextStyle.copyWith(
@@ -80,10 +82,27 @@ class ChatPage extends StatelessWidget {
       );
     }
 
+    Widget content() {
+      return Expanded(
+        child: Container(
+            width: double.infinity,
+            color: backgroundColor3,
+            child: ListView(
+              padding: EdgeInsets.symmetric(
+                horizontal: defaultMargin,
+              ),
+              children: [
+                ChatTile(),
+              ],
+            )),
+      );
+    }
+
     return Column(
       children: [
         header(),
         content(),
+        // emptyChat(),
       ],
     );
   }
