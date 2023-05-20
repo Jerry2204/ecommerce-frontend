66ffa83bf34ea0cce7d4d6df32275404889f44e2
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2023-05-20 19:47:20 +0700

    profile page header

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -4,13 +4,61 @@ import 'package:shamo/theme.dart';
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Profile Page',
-          style: primaryTextStyle,
-        ),
-      ),
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+            child: Container(
+          padding: EdgeInsets.all(
+            defaultMargin,
+          ),
+          child: Row(
+            children: [
+              ClipOval(
+                child: Image.asset(
+                  'assets/image_profile.png',
+                  width: 64,
+                ),
+              ),
+              SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hallo, Jerry',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 24,
+                        fontWeight: semibold,
+                      ),
+                    ),
+                    Text(
+                      '@jerryandriantopunkrib',
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Image.asset(
+                'assets/button_exit.png',
+                width: 20,
+              ),
+            ],
+          ),
+        )),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+      ],
     );
   }
 }
