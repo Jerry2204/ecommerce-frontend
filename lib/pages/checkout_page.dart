6facdd47cd256d3558c4ac94e52e91ce2a9b8ce4
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2024-08-06 08:32:26 +0700

    finishing cart page

---
+++ lib/pages/checkout_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class CheckoutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor3,
+    );
+  }
+}
