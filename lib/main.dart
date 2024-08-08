449a9c49da506e787fdfb7bfd551bbf0ad65b859
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2024-08-08 15:29:19 +0700

    get all data product

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:shamo/pages/signin_page.dart';
 import 'package:shamo/pages/signup_page.dart';
 import 'package:shamo/pages/splash_page.dart';
 import 'package:shamo/providers/auth_provider.dart';
+import 'package:shamo/providers/product_provider.dart';
 import 'theme.dart';
 
 void main() => runApp(MyApp());
@@ -22,6 +23,9 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(
           create: (context) => AuthProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ProductProvider(),
         ),
       ],
       child: MaterialApp(
